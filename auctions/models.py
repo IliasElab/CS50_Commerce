e304dd755c4527404949a8f0f3345d226447739d
IliasElab <elab.ilias@gmail.com>
2021-05-15 15:36:55 +0200

    Watchlist Page, Model Changes with FK Bid and Close auction

--- auctions/models.py
+++ auctions/models.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-#LIER AUCTION AVEC SON OWNER
 
 
 class Auction(models.Model):
     isactive = models.BooleanField(default = True)
-    price = models.FloatField(max_length=20)
+    price = models.ForeignKey("Bid", on_delete=models.CASCADE, related_name="bid_auction", null=True)
     title = models.CharField(max_length= 100)
     description = models.TextField(max_length=1500)
     category = models.CharField(max_length= 50, default=None)
@@ -17,9 +16,34 @@ class User(AbstractUser):
     watchlist = models.ManyToManyField(Auction, blank=True, related_name = "auctions")
 
 class Bid(models.Model):
-    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="auction_bids")
     value = models.IntegerField()
     proposer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="proposer_bids")
 
+    def __str__(self):
+        return "Bid N° "+ str(self.id)
+
 class Comment(models.Model):
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")
+
+
+# old models
+# class Auction(models.Model):
+#     isactive = models.BooleanField(default = True)
+#     baseprice = models.FloatField(max_length=20)
+#     price = models.FloatField(max_length=20)
+#     title = models.CharField(max_length= 100)
+#     description = models.TextField(max_length=1500)
+#     category = models.CharField(max_length= 50, default=None)
+#     image = models.URLField(max_length=200, default=None, blank=True)
+#     createdby = models.ForeignKey("User", on_delete=models.CASCADE , related_name="owning")
+
+# class User(AbstractUser):
+#     watchlist = models.ManyToManyField(Auction, blank=True, related_name = "auctions")
+
+# class Bid(models.Model):
+#     auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="auction_bids")
+#     value = models.IntegerField()
+#     proposer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="proposer_bids")
+
+# class Comment(models.Model):
+#     auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")
--- auctions/views.py
+++ auctions/views.py
@@ -3,13 +3,24 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import *
+#from django.forms import NameForm
+
 
 
 def index(request):
     return render(request, "auctions/index.html", {
-        "listings": Auction.objects.filter(isactive = True)
+        "listings": Auction.objects.filter(isactive = True),
+        "watchlist": False
+    })
+
+def watchlist(request):
+    wlist = request.user.watchlist.all()
+    return render(request, "auctions/index.html", {
+        "listings": Auction.objects.filter(id__in = [wl.id for wl in wlist]),
+        "watchlist": True
     })
 
 
@@ -32,7 +43,7 @@ def login_view(request):
     else:
         return render(request, "auctions/login.html")
 
-
+@login_required
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
@@ -64,27 +75,25 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
-
+@login_required()
 def addlisting(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            new = Auction(
-                title=request.POST["title"], 
-                category = request.POST["category"], 
-                description= request.POST["description"], 
-                price=request.POST["baseprice"], 
-                image=request.POST["URL"],
-                createdby=request.user)
+    if request.method == "POST":
+        initialbid = Bid(value = request.POST["baseprice"], proposer = request.user)
+        new = Auction(
+            title=request.POST["title"], 
+            category = request.POST["category"], 
+            description= request.POST["description"], 
+            price=initialbid,
+            image=request.POST["URL"],
+            createdby=request.user)
 
-
-            new.save()
-            return render(request, "auctions/index.html", {
-                "listings": Auction.objects.filter(isactive = True)
-            })
-        else:
-            return render(request, "auctions/new_listing.html")
+        initialbid.save()
+        new.save()
+        return render(request, "auctions/index.html", {
+            "listings": Auction.objects.filter(isactive = True)
+        })
     else:
-        return render(request, "auctions/login.html")
+        return render(request, "auctions/new_listing.html")
 
  
 def listing(request, listing_id):
@@ -96,6 +105,15 @@ def listing(request, listing_id):
             request.user.watchlist.add(auction)
         elif "Removing" in request.POST:
             request.user.watchlist.remove(auction)
+        elif "bid" in request.POST and float(request.POST["bid"]) > Auction.objects.get(id = listing_id).price.value:
+            new = Bid(value = float(request.POST["bid"]), proposer = request.user)
+            new.save()
+            auction.price = new
+            auction.save()
+        elif "Closing" in request.POST:
+            #return HttpResponse(auction.price)
+            auction.isactive = False
+            auction.save()
         else:
             return HttpResponse("Done erroring")
 
