5c38a66b2c13689614f99ee2892d066c74c65c02
IliasElab <elab.ilias@gmail.com>
2021-05-27 19:15:21 +0200

    Favicon + changing close auction

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
